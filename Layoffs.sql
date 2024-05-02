178bca5dba6f8fc804fe5efe7126967c676499fb
Saeid Khademi <s_khademi@ymail.com>
2024-05-01 19:57:21 -0700

    Create Layoffs.sql

---
+++ Layoffs.sql
@@ -0,0 +1,21 @@
+-- Layoffs Data from 2020-2024 
+-- This dataset was scraped from Layoffs.fyi with the hope to enable Kaggle community to look into analyzing recent mass layoffs and discover useful insights and patterns.
+-- Original dataset can be tracked at https://layoffs.fyi/
+-- Credits: Roger Lee
+
+
+
+-- List of countries total layoffs per year
+SELECT 
+    strftime('%Y', date) AS Year,
+    Country,
+    SUM(Laid_Off_Count) AS Total_Layoffs
+FROM 
+    Layoffs_Data..Layoffs
+GROUP BY 
+   strftime('%Y', date),  Country
+
+--
+
+
+

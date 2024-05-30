022f0a96a98ee7104a901697823de0ec7855dcd8
Nick S. Khademi <s_khademi@ymail.com>
2024-05-30 11:27:06 -0700

    Update Layoffs.sql

--- Layoffs.sql
+++ Layoffs.sql
@@ -1,3 +1,4 @@
+
 -- Layoffs Data from 2020-2024 
 -- This dataset was scraped from Layoffs.fyi with the hope to enable Kaggle community to look into analyzing recent mass layoffs and discover useful insights and patterns.
 -- Original dataset can be tracked at https://layoffs.fyi/
@@ -5,7 +6,9 @@
 
 
 
+
 -- List of countries total layoffs per year
+
 SELECT 
     strftime('%Y', date) AS Year,
     Country,
@@ -13,22 +16,24 @@ SELECT
 FROM 
     Layoffs_Data..Layoffs
 GROUP BY 
-   strftime('%Y', date),  Country
+    strftime('%Y', date), Country;
+	
 ----------------------------------------------
 
-
 -- List of total layoffs per country
+	
 SELECT 
     Country,
     SUM(Laid_Off_Count) AS Total_Layoffs
 FROM 
     Layoffs_Data..Layoffs
 GROUP BY 
-   Country
+    Country;
+	
 ----------------------------------------------
 
-
 -- List of total layoffs per Industry
+	
 SELECT 
     Industry,
     SUM(Laid_Off_Count) AS Total_Layoffs
@@ -39,11 +44,12 @@ WHERE
 GROUP BY 
     Industry
 ORDER BY 
-    Total_Layoffs ASC
+    Total_Layoffs ASC;
+	
 ----------------------------------------------
 
-
 -- Top 5 countries with the highest total layoffs
+	
 SELECT 
     Country,
     SUM(Laid_Off_Count) AS Total_Layoffs
@@ -56,36 +62,39 @@ GROUP BY
 ORDER BY 
     Total_Layoffs DESC
 LIMIT 5;
+
 ----------------------------------------------
 
-
 -- Retrieve total layoffs per company:
+
 SELECT 
     Company,
     SUM(Laid_Off_Count) AS Total_Layoffs
 FROM 
     Layoffs_Data..Layoffs
-	WHERE Laid_Off_Count IS NOT NULL
+WHERE 
+    Laid_Off_Count IS NOT NULL
 GROUP BY 
-    Laid_Off_Count
-	ORDER BY	
-	Total_Layoffs DESC
+    Company
+ORDER BY 
+    Total_Layoffs DESC;
+
 ----------------------------------------------
 	
-
 -- Retrieve layoffs by date range:
+	
 SELECT 
     Date,
     SUM(Laid_Off_Count) AS Total_Layoffs
 FROM 
     Layoffs_Data..Layoffs
 WHERE 
-    Date BETWEEN 'start_date' AND 'end_date'; -- Adjust start_date and end_date as needed
+    Date BETWEEN 'start_date' AND 'end_date';
+
 ----------------------------------------------
 
-
-
 -- Retrieve layoffs by stage of company:
+
 SELECT 
     Stage,
     SUM(Laid_Off_Count) AS Total_Layoffs
@@ -93,10 +102,11 @@ FROM
     Layoffs_Data..Layoffs
 GROUP BY 
     Stage;
+
 ----------------------------------------------
 
-
 -- Retrieve monthly layoffs by country
+
 SELECT 
     Country,
     strftime('%Y-%m', Date) AS Month,
